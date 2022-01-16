69e01e8648b852d1f1afcbb4b105ba8726129add
brotherson67 <mitchell.brotherson@gmail.com>
2022-01-16 03:10:11 -0700

    added first route to blogRoutes

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,4 +1,13 @@
-const express = require('express');
+const router = require('express').Router();
+const { blogPosts } = require('../../model')
 
+router.get('/', (req, res) => {
+    blogPosts.findAll()
+        .then(blogPostDbData => res.json(blogPostsDbData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err)
+        })
+})
 
 module.exports = blogRoutes;
