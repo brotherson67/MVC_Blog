00dbd2a3030cbfcc8bd39db6c33168c46dc517b0
brotherson67 <mitchell.brotherson@gmail.com>
2022-01-16 01:06:51 -0700

    getting database connection refused error

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars')
-const routes = require('./controllers');
+// const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3002;
+
+const hbs = exphbs.create({ layoutsDir: path.join(__dirname, "views/layouts"), partialsDir: path.join(__dirname, "views/partials") });
+
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
