3499f4e31191591cd76c94cf6d94989793e1b105
brotherson67 <mitchell.brotherson@gmail.com>
2022-01-16 01:35:01 -0700

    added schema.sql file

---
+++ config/seeds/schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS mvc_blog;
+CREATE DATABASE mvc_blog;
+
+USE mvc_blog;
+
+DROP TABLE IF EXISTS blog_posts;
+CREATE TABLE blog_posts(
+    id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,
+    post_body TEXT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
