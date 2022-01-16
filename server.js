061a0c05e5ba236bd16ccbddfdf1f2e14e45b25e
brotherson67 <mitchell.brotherson@gmail.com>
2022-01-16 02:48:14 -0700

    replaced all node modules, and working on thecontroller routes to fix server middleware issue

--- server.js
+++ server.js
@@ -1,18 +1,40 @@
-require('dotenv').config();
 const path = require('path');
 const express = require('express');
-const exphbs = require('express-handlebars')
-// const routes = require('./controllers');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+// const helpers = require('./utils/helpers');
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
+require('dotenv').config();
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+app.use(session(sess));
 
 const hbs = exphbs.create({ layoutsDir: path.join(__dirname, "views/layouts"), partialsDir: path.join(__dirname, "views/partials") });
-
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes);
+
+app.get('/', (req, res) => {
+    res.render('homepage', {layout: 'main'});
+})
+
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log(`Now listening on port number ${PORT}`))
+  app.listen(PORT, () => console.log('Now listening'));
 });
