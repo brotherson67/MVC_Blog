180b9a9979d7dbc30098bd3607e02f9274d334d2
brotherson67 <mitchell.brotherson@gmail.com>
2022-02-28 00:14:34 -0700

    added headroutes to controllers

--- controllers/index.js
+++ controllers/index.js
@@ -4,12 +4,14 @@ const homeRoutes = require("./homeRoutes");
 const commentRoutes = require("./commentRoutes");
 const userRoutes = require("./userRoutes");
 const blogRoutes = require("./blogRoutes");
+const headRoutes = require("./headRoutes");
 
 // Creating routes
 router.use("/", homeRoutes);
 router.use("/blog", blogRoutes);
 router.use("/user", userRoutes);
 router.use("/comment", commentRoutes);
+router.use("/head", headRoutes);
 
 // fallback route
 router.use((req, res) => {
