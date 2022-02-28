7eeeb22565213d3d87db810aaaf4942f2ec87aca
brotherson67 <mitchell.brotherson@gmail.com>
2022-02-27 23:20:37 -0700

    finished adding validation to comment model

--- model/comments.js
+++ model/comments.js
@@ -3,23 +3,35 @@ const sequelize = require("../config/connection");
 
 class Comment extends Model {}
 
-Comment.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    commentText: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: "user", key: "id" },
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: { model: "blogPosts", key: "id" },
+    },
   },
-  commentText: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  user_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: { model: "blogPosts", key: "id" },
-  },
-  post_id: {},
-});
+  {
+    sequelize,
+    modelName: "comment",
+    freezeTableName: true,
+    underscored: true,
+  }
+);
 
 module.exports = Comment;
