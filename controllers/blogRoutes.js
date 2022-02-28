3c6b34476ed85c84f3cfc68066484124e0cf7698
brotherson67 <mitchell.brotherson@gmail.com>
2022-02-27 23:55:50 -0700

    made blogRoutes post route async

--- controllers/blogRoutes.js
+++ controllers/blogRoutes.js
@@ -44,20 +44,17 @@ router.get("/:id", async (req, res) => {
 });
 
 // POST
-router.post("/", (req, res) => {
-  blogPost
-    .create({
+router.post("/", async (req, res) => {
+  try {
+    const response = await Post.create({
       title: req.body.title,
-      post_body: req.body.post_body,
-      user_id: req.body.user_id,
-    })
-    .then((dbPostData) => {
-      res.json(dbPostData);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
+      content: req.body.content,
+      user_id: req.session.user_id,
     });
+    res.json(response);
+  } catch (err) {
+    res.status(500);
+  }
 });
 
 module.exports = router;
