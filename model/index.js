c7ca75906812bf8264755c880259ce01107bec53
brotherson67 <mitchell.brotherson@gmail.com>
2022-02-27 23:24:43 -0700

    updated model/index with the associations and exported comment

--- model/index.js
+++ model/index.js
@@ -1,17 +1,35 @@
-const blogPost = require('./blogPosts')
-const User = require('./user');
+const blogPost = require("./blogPosts");
+const User = require("./user");
+const Comment = require("./comments");
 
 //associations
 User.hasMany(blogPost, {
-    foreignKey: 'user_id'
+  foreignKey: "user_id",
 });
 
 blogPost.belongsTo(User, {
-    foreignKey: 'user_id',
-    onDelete: 'SET NULL'
-})
+  foreignKey: "user_id",
+  onDelete: "SET NULL",
+});
+
+blogPost.hasMany(Comment, {
+  foreignKey: "post_id",
+});
+
+Comment.belongsTo(blogPost, {
+  foreignKey: "post_id",
+});
+
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+});
+
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+});
 
 module.exports = {
-    blogPost,
-    User
-}
+  blogPost,
+  User,
+  Comment,
+};
