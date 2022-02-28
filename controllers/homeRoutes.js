e24965826ed055fd4aa9154a248a36d63eda67cd
brotherson67 <mitchell.brotherson@gmail.com>
2022-02-27 23:45:55 -0700

    added additional routes to homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -7,10 +7,29 @@ router.get("/", async (req, res) => {
   try {
     const response = await Post.findAll({
       attributes: ["id", "title", "content", "user_id", "createdAt"],
+      include: [
+        { model: User, attributes: ["username"] },
+        { model: Comment, include: { model: User } },
+      ],
+      order: [["id", "DESC"]],
+    });
+    const posts = response.map((post) => post.get({ plain: true }));
+    res.render("homepage", {
+      posts,
+      loggedIn: req.session.loggedIn,
+      home: true,
     });
   } catch (err) {
     res.status(500);
   }
+});
+
+router.get("/login", (req, res) => {
+  res.render("login", { login: true });
+});
+
+router.get("/signup", (req, res) => {
+  res.render("signup");
 });
 
 module.exports = router;
