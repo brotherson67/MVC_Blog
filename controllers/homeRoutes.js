d150d911f28127d21b8a3d90988bff3d751a4228
brotherson67 <mitchell.brotherson@gmail.com>
2022-03-01 00:23:42 -0700

    fixed syntax error

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,20 +4,79 @@ const sequelize = require("../config/connection");
 const { blogPosts, User, Comment } = require("../model");
 
 router.get("/", (req, res) => {
-  console.log("You're getting the homepage");
-  res.render("homepage", {
-    id: 1,
-    post_url: "https://handlebarsjs.com/guide/",
-    title: "Hanglebars Docs",
-  });
+  console.log(req.session);
+
+  Post.findAll({
+    attributes: ["id", "title", "post_body"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("homepage", {
+        posts,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get("/login", (req, res) => {
-  res.render("login", { login: true });
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
 });
 
 router.get("/signup", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
   res.render("signup");
+});
+
+router.get("/post/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "title", "post_body"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+
+      // serialize the data
+      const post = dbPostData.get({ plain: true });
+
+      // pass data to template
+      res.render("single-post", {
+        post,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
