49b955b057944e1db0cc54179edf8e78689f2de6
brotherson67 <mitchell.brotherson@gmail.com>
2022-03-01 00:15:17 -0700

    updated dashboard routes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,25 +1,35 @@
 const { Comment, Post, User } = require("../model");
-const withAuth = require("../utils/auth");
+const Auth = require("../utils/auth");
+const sequelize = require("../config/connection");
 const router = require("express").Router();
 
 // GET /dashboard
-router.get("/", withAuth, async (req, res) => {
-  const response = await Post.findAll({
-    where: { user_id: req.session.user_id },
-    order: [["id", "DESC"]],
-    attributes: ["id", "title", "content", "user_id", "createdAt"],
-    include: { model: User },
-  });
-  const posts = response.map((post) => post.get({ plain: true }));
-  res.render("dashboard", {
-    posts,
-    loggedIn: req.session.loggedIn,
-    dashboard: true,
-  });
+router.get("/", Auth, (req, res) => {
+  Post.findAll({
+    where: {
+      // use the ID from the session
+      user_id: req.session.user_id,
+    },
+    attributes: ["id", "title", "post_body"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "commentText", "post_id", "user_id"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      // serialize data before passing to template
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("dashboard", { posts, loggedIn: true });
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
 // GET /dashboard/:id
-router.get("/:id", withAuth, async (req, res) => {
+router.get("/:id", Auth, async (req, res) => {
   const response = await Post.findOne({
     where: { id: req.params.id },
     attributes: ["id", "title", "content", "user_id", "createdAt"],
