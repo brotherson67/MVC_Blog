b784aea1ab7e36cd1ce4a5bc885d4b0a08581857
brotherson67 <mitchell.brotherson@gmail.com>
2022-02-28 23:09:52 -0700

    updated contollers/index to import api files

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,13 @@
 // Imports
 const router = require("express").Router();
 const homeRoutes = require("./homeRoutes");
-
 const dashboardRoutes = require("./dashboard-routes");
+const apiRoutes = require("./api");
 
 // Creating routes
 router.use("/", homeRoutes);
 router.use("/head", dashboardRoutes);
+router.use("/api", apiRoutes);
 
 // fallback route
 router.use((req, res) => {
