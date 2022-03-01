31633a0a14a3371c26f42f70832a7201b165d141
brotherson67 <mitchell.brotherson@gmail.com>
2022-03-01 00:00:44 -0700

    trying heroku

--- public/javascript/signup-page.js
+++ public/javascript/signup-page.js
@@ -3,7 +3,7 @@ async function signup(evt) {
   const username = document.querySelector("#username").value.trim();
   const password = document.querySelector("#password").value.trim();
   if (username && password) {
-    const response = await fetch("/api/user/signup", {
+    const response = await fetch("/api/user/", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ username, password }),
