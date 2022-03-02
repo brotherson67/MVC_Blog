98b0d94424a1cdc8cc337fd75b07c30f585ba437
brotherson67 <mitchell.brotherson@gmail.com>
2022-03-01 20:15:21 -0700

    experiment with login route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -55,7 +55,15 @@ router.post("/", (req, res) => {
     email: req.body.email,
     password: req.body.password,
   })
-    .then((dbUserData) => {})
+    .then((dbUserData) => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+
+        res.json(dbUserData);
+      });
+    })
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
@@ -63,31 +71,33 @@ router.post("/", (req, res) => {
 });
 
 // the user needs to login
-router.post("/login", async (req, res) => {
-  try {
-    const response = await User.findOne({
-      where: { username: req.body.username },
-    });
-    if (!response) {
-      res.status(404);
+router.post("/login", (req, res) => {
+  User.findOne({
+    where: {
+      email: req.body.email,
+    },
+  }).then((dbUserData) => {
+    if (!dbUserData) {
+      res.status(400).json({ message: "No user with that email address!" });
       return;
     }
-    const validPassword = response.checkPassword(req.body.password);
-    if (!validPassword) {
-      res.status(404);
-      return;
-    }
-    req.session.save(() => {
-      req.session.username = response.username;
-      req.session.user_id = response.id;
-      req.session.loggedIn = true;
-      res.json({ user: response.username, message: "You are now logged in!" });
-    });
-  } catch (err) {
-    res.status(500);
-  }
-});
 
+    const validPassword = dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: "Incorrect password!" });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+
+      res.json({ user: dbUserData, message: "You are now logged in!" });
+    });
+  });
+});
 // POST /user/signup
 router.post("/signup", (req, res) => {
   User.create({
