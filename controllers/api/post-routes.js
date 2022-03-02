b34ebb352656c8de6a8033306c3ff02ae2502d28
brotherson67 <mitchell.brotherson@gmail.com>
2022-03-01 20:22:55 -0700

    change routes for post-routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -4,47 +4,79 @@ const { Post, User, Comment } = require("../../models");
 const withAuth = require("../../utils/auth");
 
 // GET /post/
-router.get("/", (req, res) => {
-  blogPost
-    .findAll({
-      attributes: ["id", "title", "user_id"],
-      include: [
-        { model: User, attributes: [username] },
-        { model: Comment, attributes: ["commentText"] },
-      ],
-    })
+router.get("/api/posts", (req, res) => {
+  Post.findAll({
+    attributes: ["id", "title", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
     .then((dbPostData) => res.json(dbPostData))
-    .catch((err) => res.status(500).json(err));
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+
+  router.get("/:id", (req, res) => {
+    blogPost
+      .findOne({
+        where: { id: req.params.id },
+        attributes: ["id", "title", "post_body", "user_id"],
+        include: [
+          { model: User, attributes: [username] },
+          { model: Comment, attributes: ["commentText"] },
+        ],
+      })
+      .then((dbPostData) => {
+        if (!dbPostData) {
+          res.status(404).json({ message: "sorry no post found with that id" });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch((err) => res.status(500).json(err));
+  });
 });
 
-router.get("/:id", (req, res) => {
-  blogPost
-    .findOne({
-      where: { id: req.params.id },
-      attributes: ["id", "title", "post_body", "user_id"],
-      include: [
-        { model: User, attributes: [username] },
-        { model: Comment, attributes: ["commentText"] },
-      ],
-    })
+router.get("api/posts/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "title", "contents", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
     .then((dbPostData) => {
       if (!dbPostData) {
-        res.status(404).json({ message: "sorry no post found with that id" });
+        res.status(404).json({ message: "No post found with this id" });
         return;
       }
       res.json(dbPostData);
     })
-    .catch((err) => res.status(500).json(err));
-});
-
-// POST
-router.post("/", Auth, (req, res) => {
-  Post.create({
-    title: req.body.title,
-    post_content: req.body.post_body,
-    user_id: req.session.user_id,
-  })
-    .then((dbPostData) => res.json(dbPostData))
     .catch((err) => {
       console.log(err);
       res.status(500).json(err);
