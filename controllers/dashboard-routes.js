50429e77888d83351923a5270ca23b40505ce50f
brotherson67 <mitchell.brotherson@gmail.com>
2022-03-01 20:02:52 -0700

    added resolve method

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -66,27 +66,18 @@ router.get("/edit/:id", withAuth, (req, res) => {
         attributes: ["username"],
       },
     ],
-  }).then((dbPostData) => {});
-});
-
-router.get("/create/", Auth, (req, res) => {
-  Post.findAll({
-    where: {
-      // use the ID from the session
-      user_id: req.session.user_id,
-    },
-    attributes: ["id", "title", "post_body"],
-    include: [
-      {
-        model: Comment,
-        attributes: ["id", "commentText", "post_id", "user_id"],
-      },
-    ],
   })
     .then((dbPostData) => {
-      // serialize data before passing to template
-      const posts = dbPostData.map((post) => post.get({ plain: true }));
-      res.render("create-post", { posts, loggedIn: true });
+      if (dbPostData) {
+        const post = dbPostData.get({ plain: true });
+
+        res.render("edit-post", {
+          post,
+          loggedIn: true,
+        });
+      } else {
+        res.status(404).end();
+      }
     })
     .catch((err) => {
       res.status(500).json(err);
