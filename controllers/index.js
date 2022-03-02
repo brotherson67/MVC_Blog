033e1808734693d5df75cb52994e26420fd9abfa
brotherson67 <mitchell.brotherson@gmail.com>
2022-03-01 19:32:09 -0700

    removed fallback route from controllers

--- controllers/index.js
+++ controllers/index.js
@@ -1,17 +1,13 @@
 // Imports
 const router = require("express").Router();
+
 const homeRoutes = require("./homeRoutes");
 const dashboardRoutes = require("./dashboard-routes");
 const apiRoutes = require("./api");
 
 // Creating routes
 router.use("/", homeRoutes);
-router.use("/head", dashboardRoutes);
+router.use("/dashboard", dashboardRoutes);
 router.use("/api", apiRoutes);
-
-// fallback route
-router.use((req, res) => {
-  res.status(404).end();
-});
 
 module.exports = router;
